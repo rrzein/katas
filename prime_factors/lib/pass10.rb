9b9763e8ce9bac6ef7127e21a47b8de7ec362d1b
rrzein <rrzein@gmail.com>
2014-04-01 09:53:54 -0700

    Prime factors pass 10

---
+++ prime_factors/lib/pass10.rb
@@ -0,0 +1,15 @@
+class PrimeFactors
+
+  def self.generate(number, divisor = 2, result = [])
+    while number >= divisor
+      if number % divisor == 0
+        result << divisor
+        number /= divisor
+      else
+        divisor += 1
+      end
+    end
+    result
+  end
+
+end
