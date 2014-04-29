216dfb56963b4eefcbfeddc1e14a69b7fe679461
rrzein <rrzein@gmail.com>
2014-04-29 10:59:26 -0700

    Prime factors pass 15

---
+++ prime_factors/lib/pass15.rb
@@ -0,0 +1,14 @@
+class PrimeFactors
+
+  def self.generate(number, divisor = 2, result = [])
+    return result if divisor > number
+    if number % divisor == 0
+      result << divisor
+      number /= divisor
+      generate(number, divisor, result)
+    else
+      generate(number, divisor + 1, result)
+    end
+  end
+
+end
